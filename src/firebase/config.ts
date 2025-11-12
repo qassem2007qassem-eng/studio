2beace91c4e6e63f561d08b944a147604ac2a231
Kasm قاسم الحمام <qassem2007qassem@gmail.com>
2025-11-12 15:53:55 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-3129192888-fb0e8",
+  "appId": "1:131381470372:web:5d1adb1cd4c58c48843231",
+  "apiKey": "AIzaSyD6a--iv2vEUxCiPsSLuPAeZ20_gh-m2Yk",
+  "authDomain": "studio-3129192888-fb0e8.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "131381470372"
+};
