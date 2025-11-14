c574ee339d17516de928380000643b3baf2feb99
Kasm قاسم الحمام <qassem2007qassem@gmail.com>
2025-11-14 09:00:36 +0000

    Error: [2025-11-14T08:59:32.783Z]  @firebase/firestore: "Firestore (11.9

--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -3,21 +3,9 @@
 
 import { firebaseConfig } from '@/firebase/config';
 import { initializeApp, getApps, getApp, FirebaseApp } from 'firebase/app';
-import { getAuth, connectAuthEmulator } from 'firebase/auth';
-import { getFirestore, connectFirestoreEmulator } from 'firebase/firestore';
-import { getStorage, connectStorageEmulator } from 'firebase/storage';
-
-const EMULATORS_STARTED = 'EMULATORS_STARTED';
-function areEmulatorsRunning(): boolean {
-  // Use a global variable to track if emulators have been started.
-  // This is to avoid trying to connect to the emulators multiple times.
-  const global = globalThis as any;
-  if (!global[EMULATORS_STARTED]) {
-    global[EMULATORS_STARTED] = true;
-    return false;
-  }
-  return true;
-}
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
+import { getStorage } from 'firebase/storage';
 
 // IMPORTANT: DO NOT MODIFY THIS FUNCTION
 export function initializeFirebase() {
@@ -35,14 +23,6 @@ export function getSdks(firebaseApp: FirebaseApp) {
   const firestore = getFirestore(firebaseApp);
   const storage = getStorage(firebaseApp);
   
-  if (process.env.NODE_ENV === 'development' && !areEmulatorsRunning()) {
-    // These are the ports used by the Firebase emulators.
-    // localhost is the default host for the emulators.
-    connectFirestoreEmulator(firestore, 'localhost', 8080);
-    connectAuthEmulator(auth, 'http://localhost:9099');
-    connectStorageEmulator(storage, 'localhost', 9199);
-  }
-
   return {
     firebaseApp,
     auth,
