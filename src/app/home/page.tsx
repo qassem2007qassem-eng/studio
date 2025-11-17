9815ab4ce2732386ee04d19f56ddce56249f996e
Kasm قاسم الحمام <qassem2007qassem@gmail.com>
2025-11-17 11:37:31 +0000

    ReferenceError: CardFooter is not defined     at PostSkeleton (webpack-i

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -15,7 +15,7 @@ import Link from "next/link";
 import { Button } from "@/components/ui/button";
 import { getFeedPosts } from "@/services/post-service";
 import { type DocumentSnapshot } from "firebase/firestore";
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
 import { LogIn, Edit } from "lucide-react";
 
 function PostSkeleton() {
